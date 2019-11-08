d52713c2fbd9f98e372d34a7bc1321aa33158257
Anwar143134 <56352151+Anwar143134@users.noreply.github.com>
2019-11-08 16:39:13 +0530

    Add files via upload

---
+++ UnderWriter/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import (InsuredcreateView,PolicyCreateView,PolicyUpdateView,Pol)
+app_name = 'UnderWriter'
+
+urlpatterns = [
+    path('CreateInsured/', InsuredcreateView, name='CreateInsured'),
+    path('PolicyCreate/', PolicyCreateView, name='PolicyCreate'),
+    path('PolicyUpdate/', PolicyUpdateView, name='PolicyUpdate'),
+    path('PolicyBillPay/', PolicyBillsview, name='PolicyBillPay'),
+    ]
